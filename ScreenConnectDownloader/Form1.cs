a7f5cee6b3a7cc818f67718c55305e0b87922307
Justin Shafer <justinshafer@gmail.com>
2025-06-24 10:55:01 -0500

    Force TLS 1.2

--- ScreenConnectDownloader/Form1.cs
+++ ScreenConnectDownloader/Form1.cs
@@ -1,22 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 using System.IO.Compression;
 using System.Net;
-using System.Text;
 using System.Text.RegularExpressions;
 using System.Web.Script.Serialization;
 using System.Collections;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 
 
 namespace ScreenConnectDownloader
@@ -30,6 +22,7 @@ namespace ScreenConnectDownloader
 
         private void btnDownload_Click(object sender, EventArgs e)
         {
+            System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls12;
             string host = "https://remote.onsitedentalsystems.com";
             string supportCode = txtSupportCode.Text.Trim();
 
